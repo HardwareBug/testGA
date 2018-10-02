f0380130048606a10b3bccbc1f691427644f4c03
HardwareBug <yk1996621@gmail.com>
2018-10-02 16:24:56 +0900

    Add crossover rate

--- temp.cpp
+++ temp.cpp
@@ -6,6 +6,7 @@ const unsigned int GENOME_SIZE = 32;
 const unsigned int POPULATION_SIZE = 20;
 const unsigned int SELECTED_POPULATION_SIZE = 2;
 const unsigned int GENERATION_NUM = 5;
+const double CROSSOVER_RATE = 0.95;
 const double MUTATION_RATE = 0.05;
 
 typedef std::vector<bool> genome_t;
@@ -130,18 +131,23 @@ class Crossover {
 		//ans.resize(0);
 		//std::cout << ans.population.size() << std::endl;
 		//while (ans.size() < POPULATION_SIZE) {
-		for (unsigned int i = 0; i < arg[i].genome.size(); i++) {
-			if (rand() % 2 == 1) {
-				//std::cout << "1" << std::endl;
-				//a.genome[i] = arg[0].genome[i];
-				//b.genome[i] = arg[1].genome[i];
+		if (rand() % 10000 < crossoverRate * 10000) {
+			for (unsigned int i = 0; i < arg[i].genome.size(); i++) {
+				if (rand() % 2 == 1) {
+					//std::cout << "1" << std::endl;
+					//a.genome[i] = arg[0].genome[i];
+					//b.genome[i] = arg[1].genome[i];
+				}
+				else {
+					//std::cout << "0" << std::endl;
+					//a.genome[i] = arg[1].genome[i];
+					//b.genome[i] = arg[0].genome[i];
+					std::swap(arg[0].genome[i], arg[1].genome[i]);
+				}
 			}
-			else {
-				//std::cout << "0" << std::endl;
-				//a.genome[i] = arg[1].genome[i];
-				//b.genome[i] = arg[0].genome[i];
-				std::swap(arg[0].genome[i], arg[1].genome[i]);
-			}
+		}
+		else {
+			std::cout << "not crossoverd" << std::endl;
 		}
 		//ans.push_back(a);
 		//ans.push_back(b);
@@ -150,6 +156,7 @@ class Crossover {
 	}
 
 	int funcID = 0;
+	double crossoverRate = CROSSOVER_RATE;
 	Population(Crossover::*func[1])(Population) = { &Crossover::temp };
 public:
 	Population done(Population arg) {
@@ -158,6 +165,10 @@ public:
 
 	void setCrossoverFuncID(int ID) {
 		funcID = ID;
+	}
+
+	void setCrossoverRate(double rate) {
+		crossoverRate = rate;
 	}
 };
 
@@ -263,6 +274,7 @@ int main() {
 	evaluation.setEvaluationFuncID(0);
 	selection.setSelectionFuncID(0);
 	crossover.setCrossoverFuncID(0);
+	crossover.setCrossoverRate(CROSSOVER_RATE);
 	mutation.setMutationFuncID(0);
 	mutation.setMutationRate(MUTATION_RATE);
 
