696cbe31ebd170e4f159d06ee205bc2a23a32592
HardwareBug <yk1996621@gmail.com>
2018-10-15 12:31:17 +0900

    change my crossover

--- temp.cpp
+++ temp.cpp
@@ -4,6 +4,7 @@
 #include <math.h>
 #include <time.h>
 #include <random>
+#include <optional>
 
 std::random_device RANDOM_DEVICE;
 std::mt19937 MT(RANDOM_DEVICE());
@@ -13,8 +14,8 @@ std::uniform_int_distribution<int> RANDOM_BOOL(0, 1);
 const unsigned int GENOME_SIZE = 100;
 const unsigned int POPULATION_SIZE = 20;
 const unsigned int GENERATION_NUM = 5;
-const unsigned int ELITE_POPULATION_SIZE = 4;
-const double MUTATION_RATE = 0.00;
+const unsigned int ELITE_POPULATION_SIZE = 2;
+const double MUTATION_RATE = 0.01;
 
 // 0 : Two-point Crossover, 1 : Uniform Crossover
 const unsigned int TABLE = 1;
@@ -26,7 +27,7 @@ const unsigned int EVALUATION = 1;
 // 0 : Temporary, 1 : ranking
 const unsigned int SELECTION = 1;
 // 0 : Temporary, 1 : Use crossover table
-const unsigned int CROSSOVER = 0;
+const unsigned int CROSSOVER = 1;
 // 0 : Temporary
 const unsigned int MUTATION = 0;
 
@@ -34,6 +35,7 @@ class Connection {
 public:
 	unsigned int node1, node2;
 	double cost;
+	Connection() {};
 	Connection(unsigned int n1, unsigned int n2, double c) {
 		node1 = n1;
 		node2 = n2;
@@ -49,7 +51,8 @@ typedef int fitness_t;
 class Individual {
 public:
 	genome_t genome;
-	fitness_t fitness;
+	std::optional<fitness_t> fitness;
+	std::optional<fitness_t> pre_fitness;
 	connections_t table, choice, cut;
 	Individual() {}
 	Individual(unsigned int genomeSize) {
@@ -88,7 +91,7 @@ public:
 			for (unsigned int j = 0; j < it->genome.size(); j++) {
 				std::cout << it->genome[j];
 			}
-			std::cout << " " << it->fitness << std::endl;
+			std::cout << " " << it->fitness.value() << std::endl;
 		}
 	}
 };
@@ -100,6 +103,7 @@ namespace Evaluation {
 	Population temp(Population arg) {
 		fitness_t count;
 		for (unsigned int i = 0; i < arg.size(); i++) {
+			arg[i].pre_fitness = arg[i].fitness;
 			count = 0;
 			for (unsigned int j = 0; j < arg[i].genome.size(); j++) {
 				if (arg[i].genome[j] == true) count++;
@@ -115,6 +119,7 @@ namespace Evaluation {
 		unsigned int count;
 
 		for (int i = 0; i < (int)arg.size(); i++) {
+			arg[i].pre_fitness = arg[i].fitness;
 			arg[i].fitness = 0;
 			for (int j = 0; j < (int)arg[i].genome.size(); j++) {
 				count = 0;
@@ -127,10 +132,10 @@ namespace Evaluation {
 				}
 				if (arg[i].genome[j] == true) {
 					if (2 < count && count < 5) {
-						arg[i].fitness += 1;
+						arg[i].fitness.value() += 1;
 					}
 					else {
-						arg[i].fitness -= 1;
+						arg[i].fitness.value() -= 1;
 					}
 				}
 			}
@@ -164,22 +169,23 @@ namespace Selection {
 			return lhs.fitness < rhs.fitness;
 		});
 
-		sum = 0;
-		for (int i = 0; i < (int)arg.size(); i++) {
-			sum = sum + i + 1;
-		}
-		std::uniform_int_distribution<int> random_temp(1, sum);
 		while (ans.size() < 2) {
+			sum = 0;
+			for (int i = 0; i < (int)arg.size(); i++) {
+				sum = sum + i + 1;
+			}
+			std::uniform_int_distribution<int> random_temp(1, sum);
 			num = random_temp(MT);
 			for (int i = 0; i < (int)arg.size(); i++) {
 				num = num - i - 1;
 				if (num <= 0) {
 					ans.push_back(arg[i]);
+					arg.erase(arg.begin() + i);
 					break;
 				}
 			}
 		}
-
+		
 		return ans;
 	}
 
@@ -193,7 +199,7 @@ namespace Crossover {
 
 	Population temp(Population arg) {
 
-		for (unsigned int i = 0; i < arg[i].genome.size(); i++) {
+		for (unsigned int i = 0; i < arg[0].genome.size(); i++) {
 			if (RANDOM_BOOL(MT) == 1) {
 
 			}
@@ -207,29 +213,110 @@ namespace Crossover {
 	}
 
 	Population useTable(Population arg) {
-		double sum, randNum;
-
-		connections_t table;
+		connections_t table, choice, cut;
 		std::copy(arg[0].table.begin(), arg[0].table.end(), back_inserter(table));
-
-		sum = 0;
-		for (int i = 0; i < (int)table.size(); i++) {
-			sum = table[i].cost;
+		
+		std::vector<std::vector<unsigned int> > nodes;
+		for(unsigned int i = 0; i < arg[0].genome.size(); i++){
+			std::vector<unsigned int> temp;
+			temp.push_back(i);
+			nodes.push_back(temp);
 		}
-		if (sum != 0) {
-			std::uniform_real_distribution<double> random_temp(0, sum);
-			randNum = random_temp(MT);
+
+		//
+		//todo (change graph partitioning algorithm)
+		//
+
+		while(nodes.size() > 2){
+			double sum, randNum;
+			sum = 0;
 			for (int i = 0; i < (int)table.size(); i++) {
-				randNum -= table[i].cost;
-				//
-				// todo
-				//
-				//if(randNum < 0){
-				//	table.erase(table.begin() + i);
-				//}
+				sum += table[i].cost;
 			}
-		}
+			if(sum != 0){
+				std::uniform_real_distribution<double> random_real_temp(0, sum);
+				randNum = random_real_temp(MT);
+				//std::cout << randNum << std::endl;
+				for (int i = 0; i < (int)table.size(); i++) {
+					randNum -= table[i].cost;
+					if(table[i].cost != 0){
+						if(randNum <= 0){
 
+							unsigned int node1_p, node2_p;
+							for(unsigned int j = 0; j < nodes.size(); j++){
+								for(unsigned int k = 0; k < nodes[j].size(); k++){
+									if( nodes[j][k] == table[i].node1){
+										node1_p = j;
+									}
+									if( nodes[j][k] == table[i].node2){
+										node2_p = j;
+									}
+								}
+							}
+							if(node1_p != node2_p){
+								if(node1_p > node2_p){
+									std::swap(node1_p, node2_p);
+								}
+								std::copy(nodes[node2_p].begin(), nodes[node2_p].end(), back_inserter(nodes[node1_p]));
+								nodes[node2_p].clear();
+								nodes.erase(nodes.begin() + node2_p);
+							}
+
+							choice.push_back(table[i]);
+							table.erase(table.begin() + i);
+							break;
+						}
+					}
+				}
+			}else{
+				std::uniform_int_distribution<int> random_int_temp(0, table.size()-1);
+				randNum = random_int_temp(MT);
+
+				unsigned int node1_p, node2_p;
+				for(unsigned int j = 0; j < nodes.size(); j++){
+					for(unsigned int k = 0; k < nodes[j].size(); k++){
+						if( nodes[j][k] == table[randNum].node1){
+							node1_p = j;
+						}
+						if( nodes[j][k] == table[randNum].node2){
+							node2_p = j;
+						}
+					}
+				}
+				if(node1_p != node2_p){
+					if(node1_p > node2_p){
+						std::swap(node1_p, node2_p);
+					}
+					std::copy(nodes[node2_p].begin(), nodes[node2_p].end(), back_inserter(nodes[node1_p]));
+					nodes[node2_p].clear();
+					nodes.erase(nodes.begin() + node2_p);
+				}
+
+				choice.push_back(table[randNum]);
+				table.erase(table.begin() + randNum);
+			}
+			/*
+			for (unsigned int i = 0; i < nodes.size(); i++) {
+				for (unsigned int j = 0; j < nodes[i].size(); j++) {
+					std::cout << nodes[i][j] << " ";
+				}
+				std::cout << std::endl;
+			}
+			std::cout << std::endl;
+			*/
+		}
+		
+		for (unsigned int i = 0; i < nodes[0].size(); i++) {
+			std::swap(arg[0].genome[nodes[0][i]], arg[1].genome[nodes[0][i]]);
+		}
+		
+		for (unsigned int i = 0; i < nodes.size(); i++) {
+			for (unsigned int j = 0; j < nodes[i].size(); j++) {
+				std::cout << nodes[i][j] << " ";
+			}
+			std::cout << std::endl;
+		}
+		std::cout << std::endl;
 		return arg;
 	}
 
@@ -286,6 +373,11 @@ int main() {
 		nextPopulation.resize(POPULATION_SIZE);
 
 		population = evaluation(nextPopulation);
+		
+		//
+		//todo (add UpdateCrossoverTable)
+		//
+		
 		std::cout << std::endl;
 		std::cout << "generation " << k + 1 << " :" << std::endl;
 		population.show();
