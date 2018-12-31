da067b94a8051f74f6fe37b0c5c60a5d28e72280
HardwareBug <43433898+HardwareBug@users.noreply.github.com>
2018-12-31 15:48:30 +0900

    a

--- temp.cpp
+++ temp.cpp
@@ -6,23 +6,28 @@
 
 #include <vector>
 #include <iostream>
+#include <fstream>
 #include <algorithm>
 #include <math.h>
 #include <time.h>
 #include <random>
 #include <optional>
+#include <time.h>
 
-std::random_device RANDOM_DEVICE;
-std::mt19937 MT(RANDOM_DEVICE());
+//std::random_device RANDOM_DEVICE;
+//std::mt19937 MT(RANDOM_DEVICE());
+std::mt19937 MT((unsigned)time(NULL));
+
 std::uniform_real_distribution<double> RANDOM_RATE(0.0000, 1.0000);
 std::uniform_int_distribution<int> RANDOM_BOOL(0, 1);
-
 std::uniform_real_distribution<double> RANDOM_TABLE(0.0, 0.1);
 
-const unsigned int GENOME_SIZE = 100;
+const unsigned int EXPERIMENT_NUM = 5;
+
+const unsigned long GENOME_SIZE = 100;
 const unsigned int POPULATION_SIZE = 20;
 const unsigned int GENERATION_NUM = 100;
-const unsigned int ELITE_POPULATION_SIZE = 2;
+const unsigned int ELITE_POPULATION_SIZE = 0;
 const double MUTATION_RATE = 0.01;
 
 const unsigned int UPDATE_TABLE_DATASIZE_LINE = 100;
@@ -34,12 +39,12 @@ const unsigned int USE_UPDATE_TABLE = 1;
 const unsigned int TABLE = 1;
 const double TABLE_INIT_COST = 1;
 
-// 0 : Temporary, 1 : LifeGame (my function)
+// 0 : Temporary, 1 : LifeGame, 2 : trap 
 const unsigned int EVALUATION = 1;
 // 0 : Temporary, 1 : ranking
 const unsigned int SELECTION = 1;
 // 0 : Temporary, 1 : Use crossover table
-const unsigned int CROSSOVER = 1;
+const unsigned int CROSSOVER = 0;
 // 0 : Temporary
 const unsigned int MUTATION = 0;
 
@@ -59,7 +64,7 @@ public:
 	bool has_data;
 	
 	Individual() {}
-	Individual(unsigned int genomeSize) {
+	Individual(unsigned long genomeSize) {
 		table.resize(GENOME_SIZE, GENOME_SIZE);
 		connect.resize(GENOME_SIZE, GENOME_SIZE);
 		cut.resize(GENOME_SIZE, GENOME_SIZE);
@@ -100,7 +105,7 @@ public:
 
 			for (int j = 0; j < genomeSize; j++) {
 				if (TABLE == 0) {
-					if (std::abs(i - j) == 1 || std::abs(i - j) == GENOME_SIZE - 1) {
+					if (std::abs(i - j) == 1 || std::abs(i - j) == genomeSize - 1) {
 						table(i, j) = TABLE_INIT_COST;
 					}else{
 						table(i, j) = 0;
@@ -129,17 +134,53 @@ public:
 			this->push_back(Individual(genomeSize));
 		}
 	}
-	void show() {
+	std::string show() {
+		std::string str = "";
+		
+		std::optional<fitness_t> temp;
+		
 		for (population_t::iterator it = population_t::begin(); it != population_t::end(); it++) {
-			for (unsigned int j = 0; j < it->genome.size(); j++) {
-				std::cout << it->genome[j];
-			}
-			if(it->pre_fitness.has_value()){
-				std::cout << " " << it->fitness.value() << " " << it->pre_fitness.value() << std::endl;
+			if(!temp.has_value()){
+				temp = it->fitness.value();
 			}else{
-				std::cout << " " << it->fitness.value() << std::endl;
+				if(temp < it->fitness.value()) temp = it->fitness.value();
 			}
 		}
+		
+		//std::cout << temp.value() << " , ";
+		str = str + std::to_string(temp.value()) + " , ";
+		
+		return str;
+	}
+	
+	std::string show_genome() {
+		std::string str = "";
+		
+		for (population_t::iterator it = population_t::begin(); it != population_t::end(); it++) {
+			for (int i = 0; i < GENOME_SIZE; i++) {
+				str = str + std::to_string(it->genome[i]);
+				std::cout << it->genome[i];
+			}
+			str = str + "\n";
+			std::cout << " " << it->fitness.value();
+			std::cout << std::endl;
+		}
+		
+		return str;
+	}
+	
+	std::string show_table(){
+		population_t::iterator it = population_t::begin();
+		std::string str = "";
+		
+		for (unsigned long j = 0; j < GENOME_SIZE; j++) {
+			for (unsigned long i = 0; i < GENOME_SIZE; i++) {
+				str = str + std::to_string(it->table(i, j)) + " , ";
+			}
+			str = str + "\n";
+		}
+		
+		return str;
 	}
 };
 
@@ -190,8 +231,34 @@ namespace Evaluation {
 		}
 		return arg;
 	}
+	
+	Population trap(Population arg) {
+		int size = arg[0].genome.size() / 5;
+		
+		for (int i = 0; i < (int)arg.size(); i++) {
+			int count = 0;
+			arg[i].pre_fitness.swap(arg[i].fitness);
+			
+			arg[i].fitness = 0;
+			for (int j = 0; j < (int)arg[i].genome.size(); j++) {
+				if (arg[i].genome[j] == true) {
+					if(j < size){
+						count++;
+					}else{
+						arg[i].fitness.value() += 1;
+					}
+				}
+			}
+			if(count == size){
+				arg[i].fitness.value() += size;
+			}else{
+				arg[i].fitness.value() += size - count - 1;
+			}
+		}
+		return arg;
+	}
 
-	const funcPtrVec_t call = { temp, lifeGame };
+	const funcPtrVec_t call = { temp, lifeGame, trap };
 }
 
 const funcPtr_t evaluation = Evaluation::call[EVALUATION];
@@ -205,7 +272,6 @@ namespace Selection {
 		});
 		ans = arg;
 
-		ans.resize(2);
 		return ans;
 	}
 
@@ -217,7 +283,7 @@ namespace Selection {
 			return lhs.fitness < rhs.fitness;
 		});
 
-		while (ans.size() < 2) {
+		while (arg.size() > 0) {
 			sum = 0;
 			for (int i = 0; i < (int)arg.size(); i++) {
 				sum = sum + i + 1;
@@ -262,6 +328,7 @@ namespace Crossover {
 
 	Population useTable(Population arg) {
 		Eigen::MatrixXd adjacency(GENOME_SIZE, GENOME_SIZE);
+		
 		adjacency = arg[0].table;
 		for (int i = 0; i < GENOME_SIZE; i++) {
 			for (int j = 0; j < GENOME_SIZE; j++) {
@@ -274,10 +341,10 @@ namespace Crossover {
 		
 		Eigen::MatrixXd degree(GENOME_SIZE, GENOME_SIZE);
 		degree = Eigen::MatrixXd::Zero(GENOME_SIZE, GENOME_SIZE);
-		for (int i = 0; i < GENOME_SIZE; i++) {
+		for (unsigned long i = 0; i < GENOME_SIZE; i++) {
 			double temp = 0;
 			
-			for (int j = 0; j < GENOME_SIZE; j++) {
+			for (unsigned long j = 0; j < GENOME_SIZE; j++) {
 				temp += adjacency(i, j);
 			}
 			
@@ -299,20 +366,26 @@ namespace Crossover {
 		
 		Eigen::MatrixXd degreeLeft(GENOME_SIZE, GENOME_SIZE);
 		degreeLeft = Eigen::MatrixXd::Zero(GENOME_SIZE, GENOME_SIZE);
-		for (int i = 0; i < GENOME_SIZE; i++) {
+		for (unsigned long i = 0; i < GENOME_SIZE; i++) {
 			degreeLeft(i, i) = 1 / std::sqrt(degree(i, i));
 		}
 		//std::cout << degreeLeft << std::endl << std::endl;
 		
 		Eigen::MatrixXd degreeRight(GENOME_SIZE, GENOME_SIZE);
 		degreeRight = Eigen::MatrixXd::Zero(GENOME_SIZE, GENOME_SIZE);
-		for (int i = 0; i < GENOME_SIZE; i++) {
+		for (unsigned long i = 0; i < GENOME_SIZE; i++) {
 			degreeRight(i, i) = 1 / std::sqrt(degree(i, i));
 		}
 		//std::cout << degreeRight << std::endl << std::endl;
 		
 		Eigen::MatrixXd target(GENOME_SIZE, GENOME_SIZE);
-		target = degreeLeft * laplacian * degreeRight;
+		//target = Eigen::MatrixXd::Zero(GENOME_SIZE, GENOME_SIZE);
+		//target = degreeLeft * laplacian * degreeRight;
+		for (unsigned long j = 0; j < GENOME_SIZE; j++) {
+			for (unsigned long i = 0; i < GENOME_SIZE; i++) {
+				target(i, j) = laplacian(i, j) * degreeLeft(j, j) * degreeRight(i, i);
+			}
+		}
 		//std::cout << target << std::endl << std::endl;
 		
 		Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> eig(target);
@@ -322,7 +395,7 @@ namespace Crossover {
 		//std::cout << value << std::endl << std::endl;
 		//std::cout << vector << std::endl << std::endl;
 		
-		for (unsigned int i = 0; i < GENOME_SIZE; i++) {
+		for (unsigned long i = 0; i < GENOME_SIZE; i++) {
 			if(vector(i, 1) < 0){
 				std::swap(arg[0].genome[i], arg[1].genome[i]);
 				//std::cout << i << " ";
@@ -330,8 +403,8 @@ namespace Crossover {
 		}
 		//std::cout << std::endl;
 		
-		for (unsigned int i = 0; i < GENOME_SIZE; i++) {
-			for (unsigned int j = 0; j < GENOME_SIZE; j++) {
+		for (unsigned long i = 0; i < GENOME_SIZE; i++) {
+			for (unsigned long j = 0; j < GENOME_SIZE; j++) {
 				if(i != j && (vector(i, 1) < 0) && (vector(j, 1) < 0)){
 					arg[0].connect(i, j) = 1;
 					arg[1].connect(i, j) = 1;
@@ -347,8 +420,13 @@ namespace Crossover {
 		//std::cout << arg[0].connect << std::endl << std::endl;
 		//td::cout << arg[0].cut << std::endl << std::endl;
 		
+		arg = evaluation(arg);
+		std::sort(arg.begin(), arg.end(), [](const Individual &lhs, const Individual &rhs) {
+			return (lhs.fitness) > (rhs.fitness);
+		});
+		swap(arg[0].fitness, arg[0].pre_fitness);
+		swap(arg[1].fitness, arg[1].pre_fitness);
 		arg[0].has_data = true;
-		arg[1].has_data = true;
 		
 		return arg;
 	}
@@ -447,53 +525,56 @@ public:
 		
 		return arg;
 	}
-	
-	void show(){
-		data.show();
-	}
 };
 
 const funcPtr_t mutation = Mutation::call[MUTATION];
 
 int main() {
-	//srand((unsigned)time(NULL));
-	Population population(POPULATION_SIZE, GENOME_SIZE);
-	CrossoverTable crossoverTable;
-
-	population = evaluation(population);
-	std::cout << std::endl;
-	std::cout << "generation " << 0 << " :" << std::endl;
-	population.show();
-
-	for (unsigned int k = 0; k < GENERATION_NUM; k++) {
-		Population nextPopulation;
-
-		nextPopulation = population;
-		std::sort(nextPopulation.begin(), nextPopulation.end(), [](const Individual &lhs, const Individual &rhs) {
-			return lhs.fitness > rhs.fitness;
-		});
-		nextPopulation.resize(ELITE_POPULATION_SIZE);
-		while (nextPopulation.size() < POPULATION_SIZE) {
-			Population temp;
-
-			temp = selection(population);
-			temp = crossover(temp);
-			temp = mutation(temp);
-
-			std::copy(temp.begin(), temp.end(), back_inserter(nextPopulation));
-		}
-		nextPopulation.resize(POPULATION_SIZE);
-
-		population = evaluation(nextPopulation);
-		
-		population = crossoverTable.update(population);
-		
-		std::cout << std::endl;
-		std::cout << "generation " << k + 1 << " :" << std::endl;
-		population.show();
-	}
+	std::ofstream output_fitness("fitness.txt", std::ios::app);
+	std::ofstream output_table("table.txt", std::ios::app);
 	
-	std::cout << std::endl;
-	std::cout << "crossoverTable :" << std::endl;
-	crossoverTable.show();
+	for(int loop = 0; loop < EXPERIMENT_NUM; loop++){
+		Population population(POPULATION_SIZE, GENOME_SIZE);
+		CrossoverTable crossoverTable;
+
+		population = evaluation(population);
+	
+		output_fitness<<population.show();
+
+		for (unsigned int k = 0; k < GENERATION_NUM; k++) {
+			Population nextPopulation;
+
+			nextPopulation = population;
+			std::sort(nextPopulation.begin(), nextPopulation.end(), [](const Individual &lhs, const Individual &rhs) {
+				return lhs.fitness > rhs.fitness;
+			});
+			nextPopulation.resize(ELITE_POPULATION_SIZE);
+			population = selection(population);
+			
+			for (unsigned int l = 0; l < population.size() - 1; l++) {
+				if(nextPopulation.size() >= POPULATION_SIZE) break;
+				Population temp{};
+				temp.push_back(population[2*l]);
+				temp.push_back(population[2*l+1]);
+
+				temp = crossover(temp);
+				temp = mutation(temp);
+
+				std::copy(temp.begin(), temp.end(), back_inserter(nextPopulation));
+			}
+			nextPopulation.resize(POPULATION_SIZE);
+
+			population = evaluation(nextPopulation);
+			population = crossoverTable.update(population);
+		
+			output_fitness<<population.show();
+			population.show_genome();
+		}
+	
+		std::cout << std::endl;
+		output_fitness<<"\n";
+		output_table<<population.show_genome();
+	}
+	output_fitness.close();
+	output_table.close();
 }
